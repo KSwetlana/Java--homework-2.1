c48a5968e445b5edd43c6bdc47c75717ea5cfb74
Svetlana Kirichenko <ss7801865@gmail.ru>
2022-08-02 22:36:53 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("===Домашняя работа N2.1===\n");
+        int price = 200;
+        System.out.println("bonus: " + price / 20);
+        System.out.println("\n\n\n");
+    }
+}
+
